e4a85735c07051717dce5465dcf525a2d6d3213e
Slav <Sl-aj@yandex.ua>
2019-09-05 04:52:32 +0300

    add permission

--- articles/urls.py
+++ articles/urls.py
@@ -9,9 +9,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
-    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
-    path('new/', ArticleCreateView.as_view(), name='article_new'),
+    path('articles/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('articles/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('articles/<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+    path('articles/new/', ArticleCreateView.as_view(), name='article_new'),
     path('', ArticleListView.as_view(), name='article_list'),
 ]
